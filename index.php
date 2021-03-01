f081eb407f31fa3c7c97b8b611149e7f40a0a207
Assatou <assatou.sow@uvci.edu.ci>
2021-03-01 16:41:25 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Student</title>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<!------ Include the above in your HEAD tag ---------->
+</head>
+<body class="container bg-info">
+
+
+
+     <marquee behavior="" direction=""><h1 class="text-center">BIENVENUE SUR STUDENT ATTENDANCE</h1></marquee> 
+
+	
+	<div class="container">
+	<div class="row">
+	</div>
+	</div> 
+
+
+	
+    <div class="row">
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/login.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Espace etudiant</h4>
+            <p class="card-text"></p>
+          </div>
+          <div class="card-footer">
+            <a href="login.php" class="btn btn-primary">Connexion</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/register.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Espace etudiant</h4>
+            <p class="card-text"></p>
+          </div>
+          <div class="card-footer">
+            <a href="register.php" class="btn btn-primary">Inscription</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/teacher.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Espace professeur</h4>
+            <p class="card-text"></p>
+          </div>
+          <div class="card-footer">
+            <a href="loginprof.php" class="btn btn-primary">Connexion</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+	
+	
+	
+	
+	
+	
